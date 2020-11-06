95e5bb972c84fe18b54f8b84f82c7e1e886fa4fa
SWoolman <sandy@woolmans.net>
2020-11-07 00:06:16 +0100

    besoin de la 13h

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    float movementSpeed = 1;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+    }
+}
