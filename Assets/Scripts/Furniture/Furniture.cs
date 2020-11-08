918546f253bd713dd60cf91dd2ce194c40466974
FloreauLuca <lucafloreau@akeonet.com>
2020-11-08 18:00:55 +0100

    Add textManager

--- Assets/Scripts/Furniture/Furniture.cs
+++ Assets/Scripts/Furniture/Furniture.cs
@@ -6,21 +6,32 @@ using UnityEngine;
 public class Furniture : MonoBehaviour
 {
     protected PlayerInventory player = null;
+    protected TextManager textManager = null;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClipOpen;
+    [SerializeField] private AudioClip audioClipUseObject;
 
     void Start()
     {
         player = FindObjectOfType<PlayerInventory>();
-
+        textManager = FindObjectOfType<TextManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public virtual void OpenFurniture()
     {
-
+        if (audioSource)
+        {
+            audioSource.PlayOneShot(audioClipOpen);
+        }
     }
 
     public virtual void UseAnObject(SOObject obj)
     {
-
+        if (audioSource)
+        {
+            audioSource.PlayOneShot(audioClipUseObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
